34d9a21be593afeb6cbe75800481aa2c9df65f8a
JulianChaves17 <julychaves17@gmail.com>
2022-11-14 22:52:53 -0300

    se agrega rout detail

--- frontend/book-town-app/src/App.js
+++ frontend/book-town-app/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 import LogSignBar from "./components/LogSignBar";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/detail" element={<Detail />} />
+        
       </Routes>
     </>
   );
