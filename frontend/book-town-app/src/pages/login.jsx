f09ab6bb0fc3a6b7985bc719ad4b898ae625b2f1
Pseudo <v.pseudo.11@gmail.com>
2022-11-14 20:20:10 -0500

    Update: Login creation

--- frontend/book-town-app/src/pages/login.jsx
+++ frontend/book-town-app/src/pages/login.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
-function Login() {
-  return <div>login</div>;
+const login = () => {
+    return (
+        <div>login</div>
+    )
 }
 
-export default Login;
+export default login
