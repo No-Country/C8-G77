96800804fb2fd6ec0fdf123d639366e85ac1ede8
Cesar Gauna <gaunacesargabriel@gmail.com>
2022-11-14 21:24:10 -0300

    modificaciones archivos

--- frontend/book-town-app/src/components/LogSignBar.jsx
+++ frontend/book-town-app/src/components/LogSignBar.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/Logo-5x2.png";
 function NavBar() {
   return (
     <div className="bg-black h-24 flex items-center ">
-      <a href="">
+      <a href="login">
         <img className="h-20 w-26 ml-6" src={logo} alt="page-logo" />
       </a>
     </div>
---
+++ frontend/book-town-app/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      <LogSignBar />
+    </div>
+  );
+}
+
+export default Signup;
