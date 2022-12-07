ffc2f83daad258add22dffbdcd6c51adb04a4f3c
Cesar Gauna <gaunacesargabriel@gmail.com>
2022-12-07 07:45:03 -0300

    creacion de cart, account y modificaciones homepage

--- frontend/book-town-app/src/components/carousel/CarouselHome.jsx
+++ frontend/book-town-app/src/components/carousel/CarouselHome.jsx
@@ -10,7 +10,7 @@ function CarouselHome({ genre }) {
   const [books, setBooks] = useState(null);
 
   const getAllBooks = () => {
-    const url = `https://www.etnassoft.com/api/v1/get/?category=${genre}`;
+    const url = `https://pseudo-booktown.onrender.com/api/v1/books`;
     axios
       .get(url)
       .then((res) => setBooks(res.data))
@@ -61,13 +61,13 @@ function CarouselHome({ genre }) {
   };
 
   return (
-    <div className="cardContainer mx-auto w-[280px] sm:w-[565px] md:w-[680px] lg:w-[1040px] xl:w-[1050px] 2xl:w-[970px]">
+    <div className="cardContainer mx-auto w-[280px] sm:w-[565px] md:w-[680px] lg:w-[1040px] xl:w-[1050px] 2xl:w-[970px] xl::max-w-[1366px]">
       <Slider {...settings}>
         {React.Children.toArray(
           books?.map((item) => (
             <div className=" border border-white rounded overflow-hidden h-[230px] md:h-[260px] lg:h-[290px] 2xl:h-[300px] shadow-md shadow-black ">
               <div className="h-full">
-                <h1>{item.categories[0].name}</h1>
+                {/* <h1>{item.categories[0].name}</h1> */}
                 <img
                   className="w-full h-full"
                   src={item.cover}
