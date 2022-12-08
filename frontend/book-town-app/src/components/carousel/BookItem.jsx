29c249aae5b4636d234ec543ad3b94aab2f949be
Cesar Gauna <gaunacesargabriel@gmail.com>
2022-12-08 17:02:51 -0300

    agrenando botones de detalle

--- frontend/book-town-app/src/components/carousel/BookItem.jsx
+++ frontend/book-town-app/src/components/carousel/BookItem.jsx
@@ -1,27 +1,31 @@
-import axios from 'axios'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import getConfig from '../../utils/getConfig'
+import axios from "axios";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import getConfig from "../../utils/getConfig";
 
-const BookItem = ({book}) => {
-    const navigate = useNavigate()
+const BookItem = ({ book }) => {
+  const navigate = useNavigate();
 
-    const handleClick = ()=>{
-        navigate(`/detail/${book.book.id}`)
-    }
-    return (
-        <div className=" border border-white rounded overflow-hidden h-[230px] md:h-[260px] lg:h-[290px] 2xl:h-[300px] shadow-md shadow-black " onClick={handleClick}>
-            <div className="h-full">
-                {/* <h1>{item.categories[0].name}</h1> */}
-                <img
-                    className="w-full h-full"
-                    src={book.book.cover}
-                    alt={book.book.thumbnail}
-                />
-                <button>Add to cart</button>
-            </div>
-        </div>
-    )
-}
+  const handleClick = () => {
+    navigate(`/detail/${book.book.id}`);
+  };
+  return (
+    <div
+      className=" border border-white rounded overflow-hidden h-[230px] md:h-[260px] lg:h-[290px] 2xl:h-[300px] shadow-md shadow-black "
+      onClick={handleClick}
+    >
+      <div className="h-full">
+        <span className="flex justify-center bg-gray-900 py-0.5 text-white">
+          Ver Detalles
+        </span>
+        <img
+          className="w-full h-full"
+          src={book.book.cover}
+          alt={book.book.thumbnail}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default BookItem
+export default BookItem;
