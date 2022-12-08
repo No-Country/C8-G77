3d4f7cb5d9852766eac996273b297c6eb8c1e658
Cesar Gauna <gaunacesargabriel@gmail.com>
2022-12-08 13:36:02 -0300

    Cambio nombre de componentes

--- frontend/book-town-app/src/components/carousel/CarouselHome.jsx
+++ frontend/book-town-app/src/components/carousel/CarouselHome.jsx
@@ -73,7 +73,7 @@ function CarouselHome({ genre }) {
                   src={item.cover}
                   alt={item.thumbnail}
                 />
-                <button>Add to cart</button>
+                <button className="">Add to cart</button>
               </div>
             </div>
           ))
--- frontend/book-town-app/src/pages/Home.jsx
+++ frontend/book-town-app/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import CarouselHome from "../components/carousel/CarouselHome";
-import Dropdown from "../components/navbar/Dropdown";
+import Dropdown from "../components/Navbar/Dropdown";
 
 function Home() {
   const [genres, setGenres] = useState(null);
